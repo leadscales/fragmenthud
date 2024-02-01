6e84f4f4e271b2463e802a8956c11eaf91431b1f
Lead <87102884+leadscales@users.noreply.github.com>
2024-02-01 09:32:56 -0600

    Cleanup and extra user scripts

---
+++ rm_config.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+REM My dream is a world where everyone uses Powershell or Python
+
+CD /D "%~dp0"
+CD /D "..\..\cfg"
+DEL /F "frag*.txt"
---
+++ rm_config.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+# My dream is a world where everyone uses Powershell or Python
+
+cd "${0%/*}"
+cd ../../cfg
+rm ./frag*.txt
